223d346d6db3db13210a0ab59fb77c8c9676c680
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2023-03-20 11:15:27 +0530

    added ybclient fix to discover colocated tablets as well

--- java/yb-cdc/src/test/java/org/yb/cdc/TestGetTabletsApiCdc.java
+++ java/yb-cdc/src/test/java/org/yb/cdc/TestGetTabletsApiCdc.java
@@ -23,15 +23,22 @@ import org.yb.cdc.common.CDCBaseClass;
 import org.yb.cdc.util.CDCSubscriber;
 import org.yb.cdc.util.TestUtils;
 import org.yb.client.GetTabletListToPollForCDCResponse;
+import org.yb.client.ListTablesResponse;
 import org.yb.client.YBClient;
 import org.yb.YBTestRunner;
 
 import static org.yb.AssertionWrappers.*;
 
+import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.time.Duration;
-import java.util.Set;
+import java.util.*;
 
 import org.awaitility.Awaitility;
+import org.yb.client.YBTable;
+import org.yb.master.MasterDdlOuterClass;
 
 @RunWith(value = YBTestRunner.class)
 public class TestGetTabletsApiCdc extends CDCBaseClass {
@@ -45,6 +52,59 @@ public class TestGetTabletsApiCdc extends CDCBaseClass {
     statement = connection.createStatement();
     statement.execute("drop table if exists test;");
     statement.execute("create table test (a int primary key, b int);");
+  }
+
+  @Test
+  public void verifyGetTabletListApiOnColocatedTables() throws Exception {
+    statement.execute("drop database if exists colocated_db;");
+    statement.execute("create database colocated_db with colocated = true;");
+
+    final InetSocketAddress pgAddress = miniCluster.getPostgresContactPoints()
+      .get(0);
+    String url = String.format(
+      "jdbc:yugabytedb://%s:%d/%s",
+      pgAddress.getHostName(),
+      pgAddress.getPort(),
+      "colocated_db"
+    );
+    Properties props = new Properties();
+    props.setProperty("user", DEFAULT_PG_USER);
+    Connection conn = DriverManager.getConnection(url, props);
+    Statement st = conn.createStatement();
+
+    st.execute("CREATE TABLE test_1 (id INT PRIMARY KEY, name TEXT) WITH (COLOCATED = true);");
+    st.execute("CREATE TABLE test_2 (text_key TEXT PRIMARY KEY) WITH (COLOCATED = true);");
+    st.execute("CREATE TABLE test_3 (hours FLOAT PRIMARY KEY, hours_in_text VARCHAR(40)) WITH (COLOCATED = true);");
+
+    // Close statement and connection
+    st.close();
+    conn.close();
+
+    testSubscriber = new CDCSubscriber("colocated_db", "test_1", getMasterAddresses());
+    testSubscriber.createStream("proto");
+    String dbStreamId = testSubscriber.getDbStreamId();
+
+    List<String> tableIds = new ArrayList<>();
+    YBClient ybClient = testSubscriber.getSyncClient();
+
+    ListTablesResponse resp = ybClient.getTablesList();
+    for (MasterDdlOuterClass.ListTablesResponsePB.TableInfo tableInfo : resp.getTableInfoList()) {
+      if (tableInfo.getName().equals("test_1") || tableInfo.getName().equals("test_2") || tableInfo.getName().equals("test_3")) {
+        tableIds.add(tableInfo.getId().toStringUtf8());
+      }
+    }
+
+    // Now call new API on all the tables
+    String firstTable = tableIds.get(0); // Call on 2nd table all the time
+    for (String tableId : tableIds) {
+      firstTable = tableId;
+      YBTable table = ybClient.openTableByUUID(firstTable);
+      LOGGER.info("VKVK Calling API for table " + table.getName() + " with tableId " + table.getTableId());
+      GetTabletListToPollForCDCResponse response = ybClient.getTabletListToPollForCdc(table, dbStreamId, firstTable);
+      for (TabletCheckpointPair tcp : response.getTabletCheckpointPairList()) {
+        LOGGER.info("Table " + firstTable + " got tablet " + tcp.getTabletLocations().getTabletId().toStringUtf8());
+      }
+    }
   }
 
   @Test

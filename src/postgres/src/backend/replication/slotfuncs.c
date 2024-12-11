f820a29c8f2a776c119741b349d51f7aee5ccea7
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2024-12-11 13:07:36 +0530

    cherry picked commit 763a8cba272109225073988c01dea9419c515c9e manually

--- src/postgres/src/backend/replication/slotfuncs.c
+++ src/postgres/src/backend/replication/slotfuncs.c
@@ -123,6 +123,7 @@ pg_create_logical_replication_slot(PG_FUNCTION_ARGS)
 	Name		name = PG_GETARG_NAME(0);
 	Name		plugin = PG_GETARG_NAME(1);
 	bool		temporary = PG_GETARG_BOOL(2);
+	char		*yb_lsn_type = "SEQUENCE";
 
 	LogicalDecodingContext *ctx = NULL;
 
@@ -158,6 +159,7 @@ pg_create_logical_replication_slot(PG_FUNCTION_ARGS)
 			elog(ERROR, "cannot initialize logical decoding without a specified plugin");
 
 		YBValidateOutputPlugin(NameStr(*plugin));
+		YBValidateLsnType(yb_lsn_type);
 	}
 
 	check_permissions();
@@ -179,7 +181,7 @@ pg_create_logical_replication_slot(PG_FUNCTION_ARGS)
 	 */
 	ReplicationSlotCreate(NameStr(*name), true,
 						  temporary ? RS_TEMPORARY : RS_EPHEMERAL,
-						  NameStr(*plugin), CRS_NOEXPORT_SNAPSHOT, NULL);
+						  NameStr(*plugin), CRS_NOEXPORT_SNAPSHOT, NULL, yb_lsn_type);
 
 	memset(nulls, 0, sizeof(nulls));
 

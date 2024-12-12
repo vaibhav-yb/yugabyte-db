33068773a4ad6922059818fef1bb21d4e16e6477
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2024-12-12 12:13:39 +0530

    fixed compilation

--- src/yb/cdc/cdc_service.cc
+++ src/yb/cdc/cdc_service.cc
@@ -2954,7 +2954,7 @@ Status CDCServiceImpl::DeleteCDCStateTableMetadata(
       LOG(INFO) << "CDC state table entry for tablet " << tablet_id << " and streamid " << stream_id
                 << " is deleted";
 
-      RemoveXReplTabletMetrics(stream_id, *tablet_peer_result);
+      RemoveCDCTabletMetrics({{}, stream_id, tablet_id}, *tablet_peer_result);
       LOG(INFO) << "Metric object for CDC state table entry for tablet " << tablet_id
                 << " and streamid " << stream_id << " is deleted";
     }
